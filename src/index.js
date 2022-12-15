7244b65ab3f0a7d3b3e7931a53d22f42bf66a2bf
uwishema11 <uwishemaceline4@gmail.com>
2022-12-15 11:19:55 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
-import App from './app'
+import App from './App'
 import * as serviceWorker from './serviceWorker';
 import store from './Redux/store';
 import { Provider } from 'react-redux';
