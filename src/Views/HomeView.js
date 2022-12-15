76b7c1b6aa3ff286b18f4ec83186c0d3fc3bf3b7
uwishema11 <uwishemaceline4@gmail.com>
2022-12-15 11:31:23 +0200

    changes

--- src/Views/HomeView.js
+++ src/Views/HomeView.js
@@ -4,7 +4,17 @@ import React from "react";
  export const Homeview =()=>{
     return(
         <div>
-      <h1>hello react</h1>
+      <h1> shop with us</h1>
+      <p>
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in rutrum mi, 
+       sed blandit nibh. Sed id varius enim. Cras vitae malesuada sapien.
+        Morbi dictum fermentum est eu vehicula. Curabitur egestas consequat ornare.
+        Maecenas tincidunt ligula vel nisl accumsan imperdiet. Ut et nisi posuere,
+        condimentum ligula eget, egestas eros. Vivamus sed odio vel sem maximus molestie aliquam sed augue.
+        Pellentesque vulputate nulla id risus tempus lobortis. Sed in neque vulputate, euismod lacus quis, 
+        aliquam orci. Quisque scelerisque nisl id sapien efficitur luctus. Ut sed rhoncus dui.
+         Phasellus accumsan luctus interdum. Morbi consequat efficitur volutpat. Ut venenatis bibendum ligula ut porta.
+      </p>
       </div>
     )
 }
